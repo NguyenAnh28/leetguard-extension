ec958050368301e94d7ebd81bdda85a42f9994c4
NguyenAnh28 <nguyenanh.tr2006@gmail.com>
2025-06-23 10:06:00 -0500

    Detect when user is visiting another tab

--- background.js
+++ background.js
@@ -1,2 +1,11 @@
 console.log("Background script loaded");
 
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'loading' && tab.url) {
+        console.log("User is visiting", tab.url);
+
+        if (tab.url.includes('youtube.com')) {
+            console.log("BLOCKED! User is trying to visit Youtube");
+        }
+    }
+});
