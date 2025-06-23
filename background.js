f28309979bf120a675d75914292c69f23bdca643
NguyenAnh28 <nguyenanh.tr2006@gmail.com>
2025-06-23 10:35:42 -0500

    blocked page

--- background.js
+++ background.js
@@ -1,11 +1,19 @@
 console.log("Background script loaded");
 
+const blockedSites = ['youtube.com', 'facebook.com', 'twitter.com', 'reddit.com']
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'loading' && tab.url) {
         console.log("User is visiting", tab.url);
 
-        if (tab.url.includes('youtube.com')) {
-            console.log("BLOCKED! User is trying to visit Youtube");
+        const blockedSite = blockedSites.find(site => tab.url.includes(site));
+
+        
+        if (blockedSite) {
+            console.log("User is trying to visit a blocked", blockedSite)
+
+            const blockedPageUrl = chrome.runtime.getURL('blocked.html') + '?site=' + blockedSite;
+            chrome.tabs.update(tabId, {url: blockedPageUrl});
         }
     }
 });
